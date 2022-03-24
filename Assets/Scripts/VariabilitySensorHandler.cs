dcef67180a98ca90d38a320b22d613777d3adf83
Thor Garske Andresen <au576984@uni.au.dk>
2022-03-24 13:32:08 +0100

    skrrt

---
+++ Assets/Scripts/VariabilitySensorHandler.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariabilitySensorHandler : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> choices;
+
+    [SerializeField]
+    Material active;
+
+    [SerializeField]
+    Material normal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
